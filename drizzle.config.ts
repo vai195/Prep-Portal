579606c66b589f59690d1c83d99715a56ea6f5ca
vai195 <vksonnakul@gmail.com>
2024-09-01 21:46:56 -0400

    fixing turso migration errors

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,34 @@
-import type { Config } from "drizzle-kit";
+// import type { Config } from "drizzle-kit";
+// import { config } from "dotenv";
 
-export default {
+// config({ path: ".env" });
+
+// export default {
+//   dialect: "sqlite",
+//   schema: "./src/db/schema.ts",
+//   out: "./src/db/migrations",
+//   driver: "turso",
+//   dbCredentials: {
+//     url: process.env.TURSO_DATABASE_URL!,
+//     authToken: process.env.TURSO_AUTH_TOKEN!,
+//   },
+//   strict: true,
+//   verbose: true,
+// } satisfies Config;
+
+import { defineConfig } from "drizzle-kit";
+// import { config } from "dotenv";
+// config({ path: ".env" });
+
+export default defineConfig({
   dialect: "sqlite",
   schema: "./src/db/schema.ts",
   out: "./src/db/migrations",
   driver: "turso",
   dbCredentials: {
-    url: process.env.TURSO_CONNECTION as string,
-    authToken: process.env.TURSO_AUTH_TOKEN,
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
   strict: true,
   verbose: true,
-} satisfies Config;
+});
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,12 @@
-import { createClient } from "@libsql/client";
+import { createClient } from "@libsql/client/web";
 import { drizzle } from "drizzle-orm/libsql/driver";
 import * as schema from "./schema";
+// import { config } from "dotenv";
+// config({ path: ".env" });
+
 const client = createClient({
-  url: process.env.TURSO_CONNECTION as string,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
 export const db = drizzle(client, { schema });
